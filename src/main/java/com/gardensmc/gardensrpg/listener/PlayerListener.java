3715d8ecc7302c550d36d08383797851c0034b67
Ryan <exner014@umn.edu>
2023-09-16 18:43:41 -0700

    Change cast to swap hands key

--- src/main/java/com/gardensmc/gardensrpg/listener/PlayerListener.java
+++ src/main/java/com/gardensmc/gardensrpg/listener/PlayerListener.java
@@ -4,29 +4,17 @@ import com.gardensmc.gardensrpg.GardensRPG;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 
 public class PlayerListener implements Listener {
 
     @EventHandler
-    public void onRightClick(PlayerInteractEvent e) {
-        if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (e.getHand() == EquipmentSlot.HAND && holdingBlade(e.getPlayer())) {
-                GardensRPG.castActiveAbilityHandler.castActive(e.getPlayer());
-            }
+    public void onSwapHands(PlayerSwapHandItemsEvent e) {
+        if (holdingBlade(e.getPlayer())) {
+            GardensRPG.castActiveAbilityHandler.castActive(e.getPlayer());
+            e.setCancelled(true);
         }
     }
-
-//    @EventHandler
-//    public void onLeftClick(PlayerInteractEvent e) {
-//        if (e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK) {
-//            if (e.getHand() == EquipmentSlot.HAND && holdingBlade(e.getPlayer())) {
-//                GardensRPG.castActiveAbilityHandler.castActive(e.getPlayer());
-//            }
-//        }
-//    }
 
     private boolean holdingBlade(Player player) {
         return player.getInventory().getItemInMainHand().getType().toString().contains("SWORD");
