515339de8d87d4a0e5c9e4f95a755cf3d3841947
Ryan <exner014@umn.edu>
2023-09-17 21:49:34 -0700

    Use new cache method with sync

--- src/main/java/com/gardensmc/gardensrpg/util/AttributeUtil.java
+++ src/main/java/com/gardensmc/gardensrpg/util/AttributeUtil.java
@@ -5,21 +5,21 @@ import com.gardensmc.gardensrpg.database.entry.PlayerBladeBringerEntry;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+
 public class AttributeUtil {
 
     public static void syncAttributes(Player player) {
         try {
-            PlayerBladeBringerEntry playerBladeBringerEntry = GardensRPG.playerEntryCache.get(player.getUniqueId())
-                    .get().stream()
-                    .filter(PlayerBladeBringerEntry::isActive)
-                    .findFirst().orElse(null);
+            PlayerBladeBringerEntry playerBladeBringerEntry = GardensRPG.playerEntryCache.getActiveBladeBringer(player.getUniqueId());
             if (playerBladeBringerEntry != null) {
                 player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(playerBladeBringerEntry.getHp());
                 player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(playerBladeBringerEntry.getSpeed());
                 player.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(playerBladeBringerEntry.getAttack_damage());
             }
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (Exception e) {
+            GardensRPG.plugin.getLogger().log(Level.WARNING, "Failed to sync attributes for player " + player.getName(), e);
         }
     }
 }
